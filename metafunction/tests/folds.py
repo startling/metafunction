3efa88c996654764f9ee84b332c82e1659aff6f9
tim <tdixon51793@gmail.com>
2012-01-18 14:15:14 -0600

    None of the tests in `folds.py` are expected failures anymore.

--- metafunction/tests/folds.py
+++ metafunction/tests/folds.py
@@ -7,12 +7,10 @@ from operator import add, pow
 
 
 class TestFoldl(TestCase):
-    @expectedFailure
     def test_foldl_basic(self):
         """ Test that left folds basically work. """
         self.assertEqual(foldl(add, 2, [1, 2, 3]), 8)
 
-    @expectedFailure
     def test_foldl_order(self):
         """Test that left folds are applied in the correct order.
 
@@ -23,19 +21,16 @@ class TestFoldl(TestCase):
         """
         self.assertEqual(foldl(pow, 2, [1, 2, 3]), 64)
 
-    @expectedFailure
     def test_foldl1(self):
         "Test that foldl1 basically works."
         self.assertEqual(foldl1(add, [1, 2, 3]), 6)
 
 
 class TestFoldr(TestCase):
-    @expectedFailure
     def test_foldr_basic(self):
         """Test that right folds basically work."""
         self.assertEqual(foldr(add, 2, [1, 2, 3]), 8)
 
-    @expectedFailure
     def test_foldr_order(self):
         """Test that right fold are applied in the correct order.
 
@@ -46,7 +41,6 @@ class TestFoldr(TestCase):
         """
         self.assertEqual(foldr(pow, 2, [1, 2, 3]), 1)
 
-    @expectedFailure
     def test_foldr1(self):
         "Test that foldr1 basically works."
         self.assertEqual(foldr1(add, [1, 2, 3]), 6)
