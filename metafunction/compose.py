94a4b18c8a3b8f527136ddef23c2c7a1e5983e75
tim <tdixon51793@gmail.com>
2012-01-18 12:12:40 -0600

    `compose` now accepts an arbitrary number of functions.
    This should make the other composition test pass.

--- metafunction/compose.py
+++ metafunction/compose.py
@@ -2,8 +2,14 @@
 # -*- coding: utf-8 -*-
 """ Function composition! """
 
-def compose(a, b):
+def compose(*functions):
     "Given a list of functions, return a composition of them."
     def composition(*args, **kwargs):
-        return a(b(*args, **kwargs))
+        # start off by applying the arguments to the last function.
+        output = functions[-1](*args, **kwargs)
+        # iterate backwards over the list of functions (except the last).
+        for function in functions[:-1][::-1]:
+            # apply the output so far to each function. 
+            output = function(output)
+        return output
     return composition
