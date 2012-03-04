9d5a61d65e60c909b1fa47304de5fa8c570617e1
tim <tdixon51793@gmail.com>
2012-03-04 14:23:49 -0600

    Change the slice here to one single slice, not two.
    Tests pass so I guess it's the same thing. :S
    
    Thanks, papna and habnabit.

--- metafunction/compose.py
+++ metafunction/compose.py
@@ -8,7 +8,7 @@ def compose(*functions):
         # start off by applying the arguments to the last function.
         output = functions[-1](*args, **kwargs)
         # iterate backwards over the list of functions (except the last).
-        for function in functions[:-1][::-1]:
+        for function in functions[-2::-1]:
             # apply the output so far to each function. 
             output = function(output)
         return output
